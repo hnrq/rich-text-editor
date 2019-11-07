b4cb90b6c1c3c99bb916e3323be999e170740be4
Henrique Ramos <henriquealberone@outlook.com>
2019-11-07 18:56:52 -0300

    Updates

---
+++ precache-manifest.66362e4ecb7ab22ec517150595853d1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2489abf6e5839900f825fe8e8f4a0cd4",
+    "url": "alberoneramos.github.io/rich-text-editor/index.html"
+  },
+  {
+    "revision": "f9166698e1bfe2b5d2d7",
+    "url": "alberoneramos.github.io/rich-text-editor/static/css/main.f6da6609.chunk.css"
+  },
+  {
+    "revision": "6ad137c5a2779ad6a28a",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/2.cb2a88f2.chunk.js"
+  },
+  {
+    "revision": "f9166698e1bfe2b5d2d7",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/main.11a95c20.chunk.js"
+  },
+  {
+    "revision": "19b537742f0a5474bdf3",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/runtime-main.74cdfc72.js"
+  },
+  {
+    "revision": "52d38f023131c09408929ad54990bf1b",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/bold.52d38f02.svg"
+  },
+  {
+    "revision": "d7ef8adba1aa16a74b91b787cde50f45",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/code.d7ef8adb.svg"
+  },
+  {
+    "revision": "117a6d3e229a96ad0e0d0876352566e2",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/image.117a6d3e.svg"
+  },
+  {
+    "revision": "c46b9c4a4626ffe6ae65121427a37f2c",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/italic.c46b9c4a.svg"
+  },
+  {
+    "revision": "7da6f7a2623bd10a115cd15fc30b69e6",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/list.7da6f7a2.svg"
+  },
+  {
+    "revision": "3950b751bd78d75e1fbe5bb5126897d2",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/type.3950b751.svg"
+  },
+  {
+    "revision": "3a8023cee136469ea0eabe8d1edec5a3",
+    "url": "alberoneramos.github.io/rich-text-editor/static/media/underline.3a8023ce.svg"
+  }
+]);
