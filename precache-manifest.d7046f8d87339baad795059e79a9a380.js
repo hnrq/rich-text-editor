6a22f7d4af6efcc6565af7bddd3cd5463d9e3469
Henrique Ramos <henriquealberone@outlook.com>
2019-12-20 18:26:00 -0300

    Updates

---
+++ precache-manifest.d7046f8d87339baad795059e79a9a380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54d037eba86287b01f01479dc9456ce",
+    "url": "alberoneramos.github.io/rich-text-editor/index.html"
+  },
+  {
+    "revision": "06a25ccd84e627ae68d7",
+    "url": "alberoneramos.github.io/rich-text-editor/static/css/2.c4c43fc6.chunk.css"
+  },
+  {
+    "revision": "1aed5a696e3290d183e1",
+    "url": "alberoneramos.github.io/rich-text-editor/static/css/main.8b0f70d9.chunk.css"
+  },
+  {
+    "revision": "06a25ccd84e627ae68d7",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/2.efa90233.chunk.js"
+  },
+  {
+    "revision": "fc3bd1d442df62609c375e8cf34c9522",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/2.efa90233.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1aed5a696e3290d183e1",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/main.d9d093f2.chunk.js"
+  },
+  {
+    "revision": "b88ace7338ff5838f05b",
+    "url": "alberoneramos.github.io/rich-text-editor/static/js/runtime-main.fbccd64a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rich-text-editor/precache-manifest.e4fe879e42c71c2ca219387be9c66569.js"
+  "alberoneramos.github.io/rich-text-editor/precache-manifest.d7046f8d87339baad795059e79a9a380.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rich-text-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("alberoneramos.github.io/rich-text-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
