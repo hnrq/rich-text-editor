ca4d18ffac946ba224496bec8d27b8a349d75a23
Henrique Ramos <henriquealberone@outlook.com>
2020-01-10 17:49:54 -0300

    Updates

---
+++ precache-manifest.6c0800c702c2e4c8ea538f170aa264bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e51178f60e3850b966f2074f21a696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "087ba38c93cbe39bddab",
+    "url": "/static/css/2.c4c43fc6.chunk.css"
+  },
+  {
+    "revision": "968ba2ca3be37113ccd3",
+    "url": "/static/css/main.9f651c65.chunk.css"
+  },
+  {
+    "revision": "087ba38c93cbe39bddab",
+    "url": "/static/js/2.02a08438.chunk.js"
+  },
+  {
+    "revision": "83031e50e6fc28788eeb6035d821dfd6",
+    "url": "/static/js/2.02a08438.chunk.js.LICENSE"
+  },
+  {
+    "revision": "968ba2ca3be37113ccd3",
+    "url": "/static/js/main.a6f3c779.chunk.js"
+  },
+  {
+    "revision": "a6506eebe097c7b3f9c1",
+    "url": "/static/js/runtime-main.996dd274.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rich-text-editor/precache-manifest.905ce2d79f229c03ac191cc95f5f230b.js"
+  "/precache-manifest.6c0800c702c2e4c8ea538f170aa264bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rich-text-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
