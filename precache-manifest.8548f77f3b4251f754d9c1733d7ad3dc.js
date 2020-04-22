19e33d31d963e7483e1c54a0eb7a3e2e3f1d6bbd
Henrique Ramos <henrique.ramos@avenuecode.com>
2020-04-22 16:48:01 -0300

    Updates

---
+++ precache-manifest.8548f77f3b4251f754d9c1733d7ad3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5484b55431ba8de71f8216d6b3f4ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63bcd4e76c350e43a333",
+    "url": "/static/css/2.da2a2c6e.chunk.css"
+  },
+  {
+    "revision": "1dd1414e06c5be6a20c1",
+    "url": "/static/css/main.a7e6f66c.chunk.css"
+  },
+  {
+    "revision": "63bcd4e76c350e43a333",
+    "url": "/static/js/2.e9b1f05b.chunk.js"
+  },
+  {
+    "revision": "4fc9395fea021c73757c1c00e32fa98c",
+    "url": "/static/js/2.e9b1f05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd1414e06c5be6a20c1",
+    "url": "/static/js/main.ed1458df.chunk.js"
+  },
+  {
+    "revision": "c457c87c67ba03491d5f",
+    "url": "/static/js/runtime-main.86c2e7fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rich-text-editor/precache-manifest.5a689fcd2a63349913d64cebe337f9ac.js"
+  "/precache-manifest.8548f77f3b4251f754d9c1733d7ad3dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rich-text-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
