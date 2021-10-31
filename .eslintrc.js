ab42aac47af73bb71ceb5a01b36ddd8d148ce4d3
Henrique Ramos <henriquealberone@outlook.com>
2021-10-30 21:02:02 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'testing-library',
+    'jest-dom',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/coverage/**',
+    '.eslintrc.js',
+  ],
+};
